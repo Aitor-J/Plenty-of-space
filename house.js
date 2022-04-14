d481d88efd4fb2c5bb119dc79b33f8a64ba6e73a
Sam-M <71389760+SamM-64@users.noreply.github.com>
2022-04-14 10:20:42 +0200

    create house page

---
+++ house.js
@@ -0,0 +1,69 @@
+const house = [
+    {
+    name: "Gigantic Duplex On Uranus",
+    type: "Duplex",
+    desc:  "Mars is the fourth planet in the Solar System in ascending order of distance from the Sun and the second in ascending order of size and mass. Its distance from the Sun is between 1.381 and 1.666 AU (206.6 to 249.2 million kilometers), with an orbital period of 669.58 Martian days (686.71 days or 1.88 Earth years).",
+    img: "bg-[url('https://wallup.net/wp-content/uploads/2018/10/05/959833-sci-fi-futuristic-art-artwork-artistic-original-science-fiction-748x377.jpg')]",
+            
+          },
+    {
+      name: "Large condo on Mercury",
+      type: "House",
+      desc:  "Mars is the fourth planet in the Solar System in ascending order of distance from the Sun and the second in ascending order of size and mass. Its distance from the Sun is between 1.381 and 1.666 AU (206.6 to 249.2 million kilometers), with an orbital period of 669.58 Martian days (686.71 days or 1.88 Earth years).",
+      img: "bg-[url('https://pbs.twimg.com/media/Enmpzl2XMAInH2q?format=jpg&name=4096x4096')]",
+      
+      
+    },
+    {
+      name: "Huge Duplex on Pluto",
+      type: "Duplex",
+      desc: "Saturn is the sixth planet in the Solar System in order of distance from the Sun, and the second largest in size and mass after Jupiter, which is like it a gas giant planet.",
+      img: "bg-[url('https://thespaces.com/wp-content/uploads/2017/08/Sci-fi-tiny-homes-in-Marseille-Friche-de-l-escalette-ft.jpg')]",
+      
+    },
+    {
+      name: "Familly home on Venus",
+      type: "House",
+      desc: "This is the perfect house for you, come to visit it you'll love it ",
+      img: "bg-[url('https://i.pinimg.com/originals/5c/fd/a7/5cfda79b86bc36d18e0dd2f60a84883f.jpg')]",
+      
+    },
+    {
+      name: "Open Floor on Jupiter",
+      type: "House",
+      desc: "This is the perfect house for you, come to visit it you'll love it ",
+      img: "bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzUVq9Mni6GMVbwzIoIqFxTUZeh2jFdwVlNa5bE3lO3BRJh--oGigMtmrfVv3SbeJlD-c&usqp=CAU')]",
+      
+    },
+    {
+      name: "Familly Mansion on Saturn ",
+      type: "Duplex",
+      desc: "This is the perfect flat for you, come to visit it you'll love it ",
+      img: "bg-[url('https://cdna.artstation.com/p/assets/images/images/025/981/570/large/jesse-serrano-inktober-02-jpg.jpg?1630165207')]",
+      
+    },
+    {
+    name: "3D printed pod on Mars ",
+    type: "Duplex",
+    desc: "This is the perfect flat for you, come to visit it you'll love it ",
+    img: "bg-[url('https://www.telegraph.co.uk/content/dam/property/2016/11/07/mars-main_trans_NvBQzQNjv4BqNJjoeBT78QIaYdkJdEY4CnGTJFJS74MYhNY6w3GNbO8.jpg')]",
+            
+          },
+
+          {
+    name: "Large condo on Neptune ",
+    type: "Duplex",
+    desc: "This is the perfect flat for you, come to visit it you'll love it ",
+    img: "bg-[url('https://wallup.net/wp-content/uploads/2017/03/28/425164-landscape-futuristic-house-mountains-waterfall-science_fiction-digital_art.jpg')]",
+          
+        },
+        {
+    name: "Tiny Igloo on Kepler ",
+    type: "House",
+    desc: "This is the perfect flat for you, come to visit it you'll love it ",
+    img: "bg-[url(https://news.artnet.com/app/news-upload/2018/08/2011136_OS_N40-e1535032822560.jpg')]",
+                  
+                },
+  ];
+  
+export default house;
