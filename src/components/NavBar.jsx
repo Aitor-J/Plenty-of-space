ddac03cc73fe08fccb0af70631b87811677a9a48
stefankolpacoff <stefan.kolpacoff@gmail.com>
2022-04-14 16:09:42 +0200

    link homepage to home about and contact

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/self-closing-comp */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
@@ -8,7 +6,7 @@ const NavBar = () => {
   return (
     <div className="absolute z-10 w-full mt-0 ml-0 bg-transparent ">
       <div className="flex flex-row justify-center py-[2vh]">
-        <a href="#" className="self-center h-16 mr-3">
+        <a href="/" className="self-center h-16 mr-3">
           <img
             className="self-center h-16 mr-3"
             src={logo}
@@ -20,18 +18,18 @@ const NavBar = () => {
         </h1>
       </div>
       <div className="flex justify-center">
-        <div className="bg-white w-11/12 py-[0.1vh] "></div>
+        {/* <div className="bg-white w-11/12 py-[0.1vh] "></div> */}
       </div>
       <nav className="text-slate-400">
         <ul className="flex flex-row justify-around py-[4vh]">
           <li className="text-2xl duration-700 ease-in-out font-secondary hover:tracking-widest hover:text-white">
-            <Link to="/my-profile">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li className="text-2xl duration-700 ease-in-out font-secondary hover:tracking-widest hover:text-white">
-            <Link to="/my-profile">About</Link>
+            <Link to="/about">About</Link>
           </li>
           <li className="text-2xl duration-700 ease-in-out font-secondary hover:tracking-widest hover:text-white">
-            <Link to="/my-profile">Contact</Link>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
