b1a9b4ad3fdba3c71b98e06d7512e4f3029bf4f2
stefankolpacoff <stefan.kolpacoff@gmail.com>
2022-04-21 21:50:31 +0200

    map api planet name split / splice + align text to planets

--- src/components/HomeBody.jsx
+++ src/components/HomeBody.jsx
@@ -12,30 +12,27 @@ const background = {
 
 const HomeBody = () => {
   const [planets, setPlanets] = useState([]);
-  const getPlanets = () => {
+  useEffect(() => {
     axios
       .get(
         `https://www.datastro.eu/api/records/1.0/search/?dataset=donnees-systeme-solaire-solar-system-data&q=&rows=10&sort=-ordre_order&facet=ordre_order&facet=planete_planet&facet=distance_moyenne_average_distance_x10_6_km&facet=diametre_diameter_km&facet=masse_mass_x10_24_kg&facet=periode_de_revolution_an_orbital_period_year&facet=periode_de_rotation_rotation_period_h&facet=gravite_gravity_m_s2&facet=vitesse_orbitale_orbital_velocity_km_s&facet=temperature_moyenne_mean_temperature_degc`
       )
-      .then((res) => {
-        console.log(res.data.records[0].fields.planete_planet);
-        setPlanets(res.data.records[0].fields.planete_planet);
-      });
-  };
-  useEffect(() => {
-    getPlanets();
+      .then((res) => res.data.records)
+      .then((data) => setPlanets(data.slice(1)))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <div className={background.image}>
       <NavBar />
-      <div>
-        <div className="flex content-around  justify-center py-[2vh] w-full z-50 mt-[25vh] ">
+
+      <div className="items-center">
+        <div className="flex text-center grid-flow-row  content-around  justify-center my-auto p-1/5 w-full z-50 mt-[20vh]  ">
           {planetNav.map((planet, index) => (
             <Link key={index} to={planet.path}>
-              <div className="flex justify-center w-1/2 my-auto transition duration-1000 ease-in-out hover:scale-150">
+              <div className="flex justify-around transition duration-1000 ease-in-out opacity-70 hover:opacity-100 white hover:scale-150">
                 <img
-                  className="justify-around object-cover w-full h-full animate-spinslow "
+                  className="justify-center w-1/2 h-full p-0 m-0 w-object-cover animate-spinslow "
                   src={planet.img}
                   alt="logo plenty of space"
                 />
@@ -43,6 +40,15 @@ const HomeBody = () => {
             </Link>
           ))}
         </div>
+        <h1>
+          <div className="flex justify-around grid-cols-8 text-xs text-center text-white space-around font-secondary">
+            {planets.map((planet) => (
+              <div className="justify-center w-full gap-10 p-8 m-5 text-center align-middle p-1/5">
+                {planet.fields.planete_planet.split("/", [1])}
+              </div>
+            ))}
+          </div>
+        </h1>
       </div>
       <div
         className={` bg-bottom py-40 top-0 left-0 flex items-center justify-center flex-col bg-cover cursor-auto`}
@@ -80,7 +86,22 @@ const HomeBody = () => {
             className="w-full h-2.5 form-range appearance-none rounded p-0 bg-white outline-none "
           />
         </div>
+        <div className="relative duration-1000  ease-in-out z-30 bg-opacity-10 hover:bg-opacity-30 bg-transparent backdrop-blur-sm scroll-px-8 rounded-3xl mt-[8vh]">
+          <button
+            type="button"
+            className="text-sm text-white font-secondary  px-[2vw] py-[1vh] hover:tracking-widest hover:text-red-600"
+          >
+            <Link
+              to="/homebody"
+              className="duration-1000 ease-in-out -translate-x-1/2 l-0"
+            >
+              {" "}
+              DISCOVER{" "}
+            </Link>
+          </button>
+        </div>
       </div>
+
       <FooterBar />
     </div>
   );
