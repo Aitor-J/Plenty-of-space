7961b1c1efe59362b4bb31fc3926b3054a8addc2
Aitor Jurquet <aitor.jurquet@gmail.com>
2022-04-23 10:12:49 +0200

    Adding better responsive

--- src/planetNav.js
+++ src/planetNav.js
@@ -6,15 +6,24 @@ import saturn from "./assets/images/saturne.png";
 import uranus from "./assets/images/6-uranus.png";
 import neptune from "./assets/images/7-neptune.png";
 import earth from "./assets/images/earth.png";
+import mercureb from "./assets/images/1-mercure-blur.png";
+import venusb from "./assets/images/2-venus-blur.png";
+import marsb from "./assets/images/3-mars-blur.png";
+import jupiterb from "./assets/images/4-jupiter-blur.png";
+import saturnb from "./assets/images/5-saturn-blur.png";
+import uranusb from "./assets/images/6-uranus-blur.png";
+import neptuneb from "./assets/images/7-neptune-blur.png";
 
 const planetNav = [
   {
     path: "/",
     img: mercure,
+    blur: mercureb,
   },
   {
     path: "/",
     img: venus,
+    blur: venusb,
   },
   {
     path: "/",
@@ -23,23 +32,28 @@ const planetNav = [
   {
     path: "/homebody/mars",
     img: mars,
+    blur: marsb,
   },
   {
     path: "/",
     img: jupiter,
+    blur: jupiterb,
   },
   {
     path: "/",
     img: saturn,
+    blur: saturnb,
   },
   {
     path: "/",
     img: uranus,
+    blur: uranusb,
   },
 
   {
     path: "/",
     img: neptune,
+    blur: neptuneb,
   },
 ];
 
